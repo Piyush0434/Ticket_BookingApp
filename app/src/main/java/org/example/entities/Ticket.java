491dc893eb1395c5c073881b522ba0af307843e3
Piyush0434 <piyushghatage3@gmail.com>
2025-12-30 14:35:13 +0530

    Added all getters and setters and created loginuser and signup functions

--- app/src/main/java/org/example/entities/Ticket.java
+++ app/src/main/java/org/example/entities/Ticket.java
@@ -12,7 +12,65 @@ public class Ticket {
 
     private String destination;
 
-    private Date dateOfTravel;
+    private String dateOfTravel;
 
     private Train train;
+
+
+    public Ticket(String ticketId, String userId, String sources, String destination,String dateOfTravel, Train train) {
+        this.ticketId = ticketId;
+        this.userId = userId;
+        this.sources = sources;
+        this.destination = destination;
+        this.dateOfTravel = dateOfTravel;
+        this.train = train;
+    }
+
+    public String getTicketId() {
+        return ticketId;
+    }
+
+    public void setTicketId(String ticketId) {
+        this.ticketId = ticketId;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getSources() {
+        return sources;
+    }
+
+    public void setSources(String sources) {
+        this.sources = sources;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public String getDateOfTravel() {
+        return dateOfTravel;
+    }
+
+    public void setDateOfTravel(String dateOfTravel) {
+        this.dateOfTravel = dateOfTravel;
+    }
+
+    public Train getTrain() {
+        return train;
+    }
+
+    public void setTrain(Train train) {
+        this.train = train;
+    }
 }
